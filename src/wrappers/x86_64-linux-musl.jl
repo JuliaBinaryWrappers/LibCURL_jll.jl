37bfb46478a804c305ee153901464632fb641b4c
jlbuild <juliabuildbot@gmail.com>
2022-05-28 17:53:29 +0000

    LibCURL_jll build 7.83.1+1

--- src/wrappers/x86_64-linux-musl.jl
+++ src/wrappers/x86_64-linux-musl.jl
@@ -5,11 +5,10 @@ using LibSSH2_jll
 using Zlib_jll
 using nghttp2_jll
 using MbedTLS_jll
-using Kerberos_krb5_jll
 JLLWrappers.@generate_wrapper_header("LibCURL")
 JLLWrappers.@declare_library_product(libcurl, "libcurl.so.4")
 function __init__()
-    JLLWrappers.@generate_init_header(LibSSH2_jll, Zlib_jll, nghttp2_jll, MbedTLS_jll, Kerberos_krb5_jll)
+    JLLWrappers.@generate_init_header(LibSSH2_jll, Zlib_jll, nghttp2_jll, MbedTLS_jll)
     JLLWrappers.@init_library_product(
         libcurl,
         "lib/libcurl.so",
