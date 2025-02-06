f5fad703f238ba4540b337b909d99a87159201f2
jlbuild <juliabuildbot@gmail.com>
2025-02-06 00:00:38 +0000

    LibCURL_jll build 8.12.0+0

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -4,11 +4,10 @@ export libcurl
 using LibSSH2_jll
 using Zlib_jll
 using nghttp2_jll
-using MbedTLS_jll
 JLLWrappers.@generate_wrapper_header("LibCURL")
 JLLWrappers.@declare_library_product(libcurl, "libcurl-4.dll")
 function __init__()
-    JLLWrappers.@generate_init_header(LibSSH2_jll, Zlib_jll, nghttp2_jll, MbedTLS_jll)
+    JLLWrappers.@generate_init_header(LibSSH2_jll, Zlib_jll, nghttp2_jll)
     JLLWrappers.@init_library_product(
         libcurl,
         "bin\\libcurl-4.dll",
